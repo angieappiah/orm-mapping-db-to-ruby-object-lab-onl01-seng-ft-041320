1d2374d0e1334a88eb05051e42cc5f72e2eff632
Angela Appiah <alegnalawn2@yahoo.com>
2020-05-16 04:32:14 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -38,18 +38,18 @@ class Student
       self.new_from_db(row)
     end.first
   end
-  
+
 
   def save
     sql = <<-SQL
-      INSERT INTO students (name, grade) 
+      INSERT INTO students (name, grade)
       INSERT INTO students (name, grade)
       VALUES (?, ?)
     SQL
 
     DB[:conn].execute(sql, self.name, self.grade)
   end
-  
+
 
   def self.create_table
     sql = <<-SQL
@@ -94,7 +94,7 @@ class Student
     SQL
 
     s = DB[:conn].execute(sql, x)
-  
+
   end
 
   def self.first_student_in_grade_10
