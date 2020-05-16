4f565f6ce9ea84db34e1b99b410383fb4e019a81
Angela Appiah <alegnalawn2@yahoo.com>
2020-05-16 03:53:04 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -40,6 +40,12 @@ class Student
      end
    end
 
+   def self.students_below_12th_grade
+  sql = "SELECT * FROM students WHERE grade < 12"
+  DB[:conn].execute(sql)
+end
+
+
   def save
     sql = <<-SQL
       INSERT INTO students (name, grade)
