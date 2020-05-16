23a8d5efa5bbcaf4fb6c628118fa18fe26dc75d9
Angela Appiah <alegnalawn2@yahoo.com>
2020-05-16 03:49:41 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -27,6 +27,15 @@ class Student
   self.new_from_db(student_row)
   end
 
+  def self.students_below_12th_grade
+     sql = <<-SQL
+     SELECT * 
+     FROM students 
+     WHERE students.grade < 12
+     SQL
+     DB[:conn].execute(sql).collect do |row|
+       self.new_from_db(row)
+     end
 
   def save
     sql = <<-SQL
