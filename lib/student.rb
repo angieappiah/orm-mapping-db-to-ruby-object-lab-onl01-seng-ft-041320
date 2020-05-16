d49f1be074e7bc817841aee50487023438376121
Angela Appiah <alegnalawn2@yahoo.com>
2020-05-16 03:45:31 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -27,15 +27,6 @@ class Student
   self.new_from_db(student_row)
   end
 
-  def self.count_all_students_in_grade_9
-    sql = "SELECT * FROM students WHERE grade = 9"
-    DB[:conn].execute(sql)
-  end
-
-  def self.students_below_12th_grade
-   sql = "SELECT * FROM students WHERE grade < 12"
-   DB[:conn].execute(sql)
- end
 
   def save
     sql = <<-SQL
